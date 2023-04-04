44a3f9ca3d63f9d6b3b58a247fd625b3902f5d4b
anamendes23 <anamendes23@gmail.com>
2023-04-04 16:14:42 -0700

    Implemented basic server functions

--- proxy.py
+++ proxy.py
@@ -14,7 +14,7 @@ client and the response message delivered by the web server pass through the pro
 server.
 
 Usage:
-$python3 proxy.py port
+$ python3 proxy.py port
 
 """
 
@@ -22,6 +22,12 @@ from socket import *
 import sys
 from urllib.parse import urlparse
 from pathlib import Path
+
+BACKLOG = 5 # listener queue size
+HOST = 'localhost'
+BUF_SZ = 4096
+SERVER_ADDR = ('css1.seattleu.edu', 80)
+TIMEOUT = 5 # seconds
 
 class ProxyServer(object):
     """
@@ -35,16 +41,87 @@ class ProxyServer(object):
     """
 
     def __init__(self, port: int):
-        self.port = port
-        print("Proxy server")
+        self.listener, self.listener_address = self.create_socket(port)
+        self.log(f'Listening at {self.listener_address}')
+        self.start_proxy()
+    
+    def create_socket(self, port: int):
+        try:
+            listener = socket(AF_INET, SOCK_STREAM)
+            address = (HOST, port)
+            listener.bind(address)
+            listener.listen(BACKLOG)
+            return listener, address
+        except Exception as e:
+            self.log(f'Unable to connect to address ({HOST}, {port})\n{str(e)}')
+            exit(1)
+    
+    def start_proxy(self):
+        while True:
+            try:
+                self.log(f' ******************** Ready to serve ********************')
+                client, client_addr = self.listener.accept()
+                self.log(f'Received a client connection from {client_addr}')
+                self.handle_connection(client)
+                self.log(f'All done! Closing socket...\n\n')
+                client.close()
+            except Exception as e:
+                print(str(e))
+                self.log(f'\nServer is shutting down...\n')
+                exit(0)
+    
+    def handle_connection(self, client: socket):
+        # receive message
+
+        # parse message
+
+        # verify if version is supported
+
+        # verify if method is supported
+
+        # handle request based on method
+
+        self.log(f'Receiving message from client')
+        client_msg = self.receive(client)
+        self.log(client_msg.decode('utf-8'))
+        self.send(client, client_msg.decode('utf-8'))
+
+    def contact_server(self, client_request):
+        with socket(AF_INET, SOCK_STREAM) as proxy_sock:
+            proxy_sock.connect(SERVER_ADDR)
+            self.send(proxy_sock, client_request)
+            return self.receive(proxy_sock)
+    
+    def receive(self, sock):
+        sock.settimeout(TIMEOUT)
+        data = bytearray()
+        while True:
+            try:
+                temp = sock.recv(BUF_SZ)
+                # print(temp)
+                if len(temp) <= 0:
+                    break
+                data.extend(temp)
+            except Exception as e:
+                print(str(e))
+                break
+        self.log(f'Received a message from this client: {data}')
+        return data
+    
+    def send(self, sock, data):
+        sock.sendall(bytes(data, 'utf-8'))
+    
+    def log(self, message):
+        print(message)
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 proxy.py port")
+        print('Usage: python3 proxy.py port')
         exit(1)
     
-    server = ProxyServer(sys.argv[1])
+    port = int(sys.argv[1])
+    server = ProxyServer(port)
 
 
 if __name__ == '__main__':
